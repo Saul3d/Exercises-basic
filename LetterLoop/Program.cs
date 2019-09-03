1bbc4637fa5ca1ee76def58281ae09f64be69c4e
saul3d <saul3d@yahoo.com>
2019-09-03 17:43:04 -0500

    submitting first assignment

---
+++ LetterLoop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace LetterLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Console.WriteLine("input: " + input);
+            string inputWithRemovedSpaces = input.Replace(" ", "");
+            var compositechar = "";
+            for (int k = 0; k < inputWithRemovedSpaces.Length; k++)
+            {
+                int h = 0; var tmp = "";
+                while (h <= k)
+                {
+                    tmp += string.Concat((h == 0) ? (char.ToUpper(inputWithRemovedSpaces[k])) : char.ToLower(inputWithRemovedSpaces[k]));
+                    h++;
+                }
+                compositechar += tmp + '-';
+            }
+            Console.WriteLine("output: " + compositechar.Trim('-'));
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
---
+++ numberNmultipliers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace numberMultipliers
+{
+    class Program
+    {
+        static void Main(string[] args)     
+        {
+            Console.WriteLine("Would you like to Multiply or Square an array of numbers");
+            var usersOperatorChoice = Console.ReadLine().ToLower();
+
+            if (usersOperatorChoice == "multiply")
+            {
+                Console.WriteLine("Please enter a comma separated list of numbers");
+                var input = Console.ReadLine();
+                var numbers = input.Split(",");
+
+                var multiplyNumbers = 1;
+
+                for (var i = 0; i < numbers.Length; i++)
+                {
+                    multiplyNumbers *= int.Parse(numbers[i]);
+                }
+                Console.WriteLine(multiplyNumbers);
+            }
+            else if (usersOperatorChoice == "square")
+            {
+                Console.WriteLine("Please enter a comma separated list of numbers");
+                var input = Console.ReadLine();
+                var numbers = input.Split(",");
+
+
+                for (var i = 0; i < numbers.Length; i++)
+                {
+                    var numberSquared = int.Parse(numbers[i]);
+                    Console.WriteLine(numberSquared * numberSquared);
+                }
+            }
+            else
+            {
+                Console.WriteLine("You must enter \"Multiply\" or \"Square\"");
+            }
+
+        }
+    }
+}
